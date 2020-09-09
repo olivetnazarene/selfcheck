635657f9928dc8b741978a02a31af9cb6afb7093
James Cuénod <j3frea+gitkraken@gmail.com>
2020-09-08 21:57:13 -0500

    Autoindent

--- client/js/self-check.js
+++ client/js/self-check.js
@@ -2,7 +2,8 @@
 const protocol = window.location.protocol + "//";
 const hostname = window.location.hostname;
 const port = window.location.port;
-const baseURL = port ? (protocol + hostname + ":"+ port) : (protocol + hostname);	
+const baseURL = port ? (protocol + hostname + ":" + port) : (protocol + hostname);
+
 let TOUCH = false
 const configure_for_touch = () => {
 	$("#idinstruction").text("Please scan your ID:")
@@ -13,27 +14,26 @@ const configure_for_touch = () => {
 
 async function initiate() {
 	getModalBox();
-	
-	$("#barcode").bind("keypress", function(e) {
+
+	$("#barcode").bind("keypress", function (e) {
 		let code = e.keyCode || e.which;
-		if(code == 13) {
+		if (code == 13) {
 			loan();
-		 }
+		}
 	});
-	
-	$("#userid").bind("keypress", function(e) {
+
+	$("#userid").bind("keypress", function (e) {
 		let code = e.keyCode || e.which;
-		if(code == 13) {
+		if (code == 13) {
 			login();
-		 }
+		}
 	});
 
 	const istouch = await fetch("//is-touch")
 	if ("touch" in istouch) {
 		TOUCH = istouch.touch
 		configure_for_touch()
-	}
-	else {
+	} else {
 		console.error("Failed to query `/is-touch` endpoint")
 	}
 }
@@ -45,25 +45,25 @@ var sessiontimer;
 var defaultTimeout = 60
 
 function getModalBox() {
-	
+
 	// Get the modal
 	modal = document.getElementById('myModal');
 	$("#myModal").hide();
-	
+
 	// Get the <span> element that closes the modal
 	span = document.getElementsByClassName("close")[0];
 
 	// When the user clicks on <span> (x), close the modal
-	span.onclick = function() {
+	span.onclick = function () {
 		$("#myModal").hide();
 	}
 
 	// When the user clicks anywhere outside of the modal, close it
 	/*
 	window.onclick = function(event) {
-	    if (event.target == modal) {
-	    	$("#myModal").hide();
-	    }
+		if (event.target == modal) {
+			$("#myModal").hide();
+		}
 	}
 	*/
 }
@@ -71,7 +71,7 @@ function getModalBox() {
 function returnToBarcode() {
 	$("#barcode").prop("disabled", false);
 	$("#myModal").hide();
-	
+
 	$("#barcode").val("");
 	$("#barcode").focus();
 }
@@ -80,24 +80,24 @@ function returnToBarcode() {
 /* LOGIN */
 
 function login() {
-    let loginid = $("#userid").val();
-    if ((loginid != null) && (loginid != "")) {
-    	
-    	$("#userid").prop("disabled", true);
-    	$("#loginerror").addClass("hide");
-    	
-    	$("#modalheader").text("Loading data, please wait...");
-        $("#myModal").show();
-        $(".close").hide();
-				
-				// console.log(baseURL+"/users/"+loginid)
-        $.ajax({
-    		type: "GET",
-    		url: baseURL+"/users/"+$("#userid").val(),
+	let loginid = $("#userid").val();
+	if ((loginid != null) && (loginid != "")) {
+
+		$("#userid").prop("disabled", true);
+		$("#loginerror").addClass("hide");
+
+		$("#modalheader").text("Loading data, please wait...");
+		$("#myModal").show();
+		$(".close").hide();
+
+		// console.log(baseURL+"/users/"+loginid)
+		$.ajax({
+			type: "GET",
+			url: baseURL + "/users/" + $("#userid").val(),
 			contentType: "text/plain",
-			dataType : "json",
+			dataType: "json",
 			crossDomain: false
-		}).done(function(data) {
+		}).done(function (data) {
 			user = data;
 			// console.log(JSON.stringify(user));
 			// prepare scan box
@@ -109,26 +109,26 @@ function login() {
 			// console.log(timeoutspan)
 			sessionTimeout(defaultTimeout, timeoutspan);
 			//$("#usernotes").text(data.user_note.length);
-			
-		  $("#loanstable").find("tr:gt(0)").remove();
-			
+
+			$("#loanstable").find("tr:gt(0)").remove();
+
 			$("#loginbox").addClass("hide");
 			$("#scanbox").toggleClass("hide");
-			
+
 			$("#barcode").focus();
 
-			
-		}).fail(function(jqxhr, textStatus, error) {
-				$("#loginerror").toggleClass("hide");
-				$("#userid").val(""); //Clear userid for touchless retry
-		    console.log(jqxhr.responseText);
-		    
-		}).always(function() {
+
+		}).fail(function (jqxhr, textStatus, error) {
+			$("#loginerror").toggleClass("hide");
+			$("#userid").val(""); //Clear userid for touchless retry
+			console.log(jqxhr.responseText);
+
+		}).always(function () {
 			$("#userid").prop("disabled", false);
 			$("#myModal").hide();
 			focusLogin();
 		});
-    }
+	}
 }
 
 function loaduser(data) {
@@ -136,59 +136,59 @@ function loaduser(data) {
 }
 
 function loan() {
-	
+
 	let barcode = $("#barcode").val();
 
-    if ((barcode != null) && (barcode != "")) {
-			if (barcode == user.user_identifier[0].value || barcode == user.primary_id){
-				console.log("Re-scanned userid, logging out")
-				logout();
-				return 
-			}
-    	console.log($("#barcode").val());
-    	$("#modalheader").text("Processing request, please wait...");
-      $("#myModal").show();
-      $(".close").hide();
-			$("#barcode").prop("disabled", true);
+	if ((barcode != null) && (barcode != "")) {
+		if (barcode == user.user_identifier[0].value || barcode == user.primary_id) {
+			console.log("Re-scanned userid, logging out")
+			logout();
+			return
+		}
+		console.log($("#barcode").val());
+		$("#modalheader").text("Processing request, please wait...");
+		$("#myModal").show();
+		$(".close").hide();
+		$("#barcode").prop("disabled", true);
 
-    	$.ajax({
-    		type: "POST",
-				url: baseURL + "/users/" + user.primary_id + "/loans?item_barcode=" + $("#barcode").val(),
-				contentType: "application/json",
-				dataType: "JSON",
-				data:``
-    	}).done(function(data){
-    		let dueDate = new Date(data.due_date);
-    		let dueDateText = (parseInt(dueDate.getMonth()) + 1) + "/" + dueDate.getDate() + "/" + dueDate.getFullYear();
-    		$("#loanstable").append("<tr><td>" + data.title + "</td><td>" + dueDateText + "</td></tr>");
-    		
-    		returnToBarcode();
-    		
-    	}).fail(function(jqxhr, textStatus, error) {
-    		console.log(jqxhr.responseText);
-    		
-    		$("#modalheader").text("");
-    		$("#modalheader").append("Item not available for loan.<br/><br/>Please see the circulation desk for more information.<br/><br/><input class='modalclose' type='button' value='close [6]' id='barcodeerrorbutton' onclick='javascript:returnToBarcode();'/>");
-    		$("#barcodeerrorbutton").focus();
-    		
-    		$(".close").show();
-				
-    		$("#barcode").val("");
-				modalTimeout();
-    	}).always(function() {
-    		extendTimeout();
-    	});
-    	
-    }
-} 
+		$.ajax({
+			type: "POST",
+			url: baseURL + "/users/" + user.primary_id + "/loans?item_barcode=" + $("#barcode").val(),
+			contentType: "application/json",
+			dataType: "JSON",
+			data: ``
+		}).done(function (data) {
+			let dueDate = new Date(data.due_date);
+			let dueDateText = (parseInt(dueDate.getMonth()) + 1) + "/" + dueDate.getDate() + "/" + dueDate.getFullYear();
+			$("#loanstable").append("<tr><td>" + data.title + "</td><td>" + dueDateText + "</td></tr>");
 
-function modalTimeout(){
+			returnToBarcode();
+
+		}).fail(function (jqxhr, textStatus, error) {
+			console.log(jqxhr.responseText);
+
+			$("#modalheader").text("");
+			$("#modalheader").append("Item not available for loan.<br/><br/>Please see the circulation desk for more information.<br/><br/><input class='modalclose' type='button' value='close [6]' id='barcodeerrorbutton' onclick='javascript:returnToBarcode();'/>");
+			$("#barcodeerrorbutton").focus();
+
+			$(".close").show();
+
+			$("#barcode").val("");
+			modalTimeout();
+		}).always(function () {
+			extendTimeout();
+		});
+
+	}
+}
+
+function modalTimeout() {
 	let timer = 5
-	let timeout = setInterval(function(){
+	let timeout = setInterval(function () {
 		let seconds = parseInt(timer % 60, 10)
 		$("#barcodeerrorbutton").val(`close [${timer}]`)
 
-		if(--timer < 0){
+		if (--timer < 0) {
 			// $(".close").hide()
 			$("#myModal").hide();
 			returnToBarcode();
@@ -197,34 +197,35 @@ function modalTimeout(){
 	}, 1000)
 }
 
-function sessionTimeout(duration, display){
+function sessionTimeout(duration, display) {
 	sessiontimer = duration;
-	let timeout = setInterval(function(){
+	let timeout = setInterval(function () {
 		let minutes = parseInt(sessiontimer / 60, 10)
 		let seconds = parseInt(sessiontimer % 60, 10)
 
-		minutes = minutes < 10 ? "0" + minutes : minutes 
-		seconds = seconds < 10 ? "0" + seconds : seconds 
-		
-		if(sessiontimer == null){
+		minutes = minutes < 10 ? "0" + minutes : minutes
+		seconds = seconds < 10 ? "0" + seconds : seconds
+
+		if (sessiontimer == null) {
 			clearInterval(timeout)
 		}
 
-		if (sessiontimer != null && --sessiontimer < 0){
+		if (sessiontimer != null && --sessiontimer < 0) {
 			// sessiontimer = duration;
 			console.log("sessiontimer is below zero, calling logout and clearInterval");
 			logout();
 			clearInterval(timeout);
 		}
-		
-		if (sessiontimer >= 0){
+
+		if (sessiontimer >= 0) {
 			// console.log(sessiontimer)
 			display.textContent = minutes + ":" + seconds;
 		}
-		
+
 	}, 1000)
 }
-function extendTimeout(){
+
+function extendTimeout() {
 	sessiontimer = defaultTimeout
 }
 
@@ -242,12 +243,12 @@ function logout() {
 	$("#barcode").val("");
 }
 
-$( document ).ready(function() {
-	  focusLogin();
-	});
+$(document).ready(function () {
+	focusLogin();
+});
 
-function focusLogin(){
-	$(document).ready(function(){
-	$( "#userid" ).focus();
+function focusLogin() {
+	$(document).ready(function () {
+		$("#userid").focus();
 	})
 }
--- main.js
+++ main.js
@@ -21,110 +21,114 @@ const config = require('./config');
 // 
 app.get('/', (req, res) => index(req, res))
 app.use(express.static('client'))
-app.get('/users/:userId', (req, res ) => {
-  console.log("user id scan");
-  getUser(req.params, res);
+app.get('/users/:userId', (req, res) => {
+	console.log("user id scan");
+	getUser(req.params, res);
 })
-app.post('/users/:userId/loans?', jsonParser, (req, res)=> {
-  console.log(req.query.item_barcode)
-  console.log(req.body)
-  requestLoan(req.params, req.query, req.body, res)
+app.post('/users/:userId/loans?', jsonParser, (req, res) => {
+	console.log(req.query.item_barcode)
+	console.log(req.body)
+	requestLoan(req.params, req.query, req.body, res)
 })
 app.get('/is-touch', (req, res) => {
-	res.json({ "touch": config.touch })
+	res.json({
+		"touch": config.touch
+	})
 })
 app.listen(port, () => console.log(`Selfcheck has started listening at ${port}`))
 
-async function getUser(params, res){
-  console.log(`Retrieving user with id ${params.userId}.`)
+async function getUser(params, res) {
+	console.log(`Retrieving user with id ${params.userId}.`)
 
-  let u = await get_api_user(params.userId)
+	let u = await get_api_user(params.userId)
 
-  if(u){
-	  res.json(u)
-  }else{
-    res.send('something went wrong with the lookup')
-  }
+	if (u) {
+		res.json(u)
+	} else {
+		res.send('something went wrong with the lookup')
+	}
 }
 
-async function requestLoan(params, query, body, res){
-  console.log(`Loan processing started ${JSON.stringify(params)} and ${JSON.stringify(query)} and ${JSON.stringify(body)}`)
-  
-  let loan = await api_request_loan(params.userId, query.item_barcode)
-  
-  if (loan.error){
-    res.send(`${loan.error[0].errorMessage}`)
-  }
-  else if(loan){
-    res.json(loan)
-  }else{
-	  res.send('something went wrong with the lookup')
-  }
+async function requestLoan(params, query, body, res) {
+	console.log(`Loan processing started ${JSON.stringify(params)} and ${JSON.stringify(query)} and ${JSON.stringify(body)}`)
+
+	let loan = await api_request_loan(params.userId, query.item_barcode)
+
+	if (loan.error) {
+		res.send(`${loan.error[0].errorMessage}`)
+	} else if (loan) {
+		res.json(loan)
+	} else {
+		res.send('something went wrong with the lookup')
+	}
 
 }
 // 
 // API calls
 // 
-function get_api_user(id){
-  const options = {
-    baseURL: config.hostname,
-    port: 443,
-    url: '/almaws/v1/users/'+id+'?expand=loans,requests,fees&format=json',
-    method: 'get',
-    headers: {Authorization: `apikey ${config.apiKey}` }
-  }
+function get_api_user(id) {
+	const options = {
+		baseURL: config.hostname,
+		port: 443,
+		url: '/almaws/v1/users/' + id + '?expand=loans,requests,fees&format=json',
+		method: 'get',
+		headers: {
+			Authorization: `apikey ${config.apiKey}`
+		}
+	}
 
-  // console.log(JSON.stringify(options))
-  const getData = async options => {
-    try {
-      const response = await axios.request(options)
-      const data = response.data
+	// console.log(JSON.stringify(options))
+	const getData = async options => {
+		try {
+			const response = await axios.request(options)
+			const data = response.data
 
-      return data
-    }catch (error){
-      console.log(error)
-    }
-  }
+			return data
+		} catch (error) {
+			console.log(error)
+		}
+	}
 
-  return getData(options)
+	return getData(options)
 }
 
-function api_request_loan(userid, barcode){
-  let library_xml = `<?xml version='1.0' encoding='UTF-8'?><item_loan><circ_desk>${config.circDesk}</circ_desk><library>${config.libraryName}</library></item_loan>`
-  const options = {
-    baseURL: config.hostname,
-    port: 443,
-    url: `/almaws/v1/users/${userid}/loans?user_id_type=all_unique&item_barcode=${barcode}`,
-    data: library_xml,
-    method: 'post',
-    headers: {
-      'Content-Type': `application/xml`,
-      Authorization: `apikey ${config.apiKey}`}
-  }
+function api_request_loan(userid, barcode) {
+	let library_xml = `<?xml version='1.0' encoding='UTF-8'?><item_loan><circ_desk>${config.circDesk}</circ_desk><library>${config.libraryName}</library></item_loan>`
+	const options = {
+		baseURL: config.hostname,
+		port: 443,
+		url: `/almaws/v1/users/${userid}/loans?user_id_type=all_unique&item_barcode=${barcode}`,
+		data: library_xml,
+		method: 'post',
+		headers: {
+			'Content-Type': `application/xml`,
+			Authorization: `apikey ${config.apiKey}`
+		}
+	}
 
-  const getData = async options => {
-    try {
-      const response = await axios.request(options)
-      const data = response.data
+	const getData = async options => {
+		try {
+			const response = await axios.request(options)
+			const data = response.data
 
-      return data
-    }catch(error){
-      if(error.response){
-        console.log(error.response.data)
-        console.log(`ErrorList ${JSON.stringify(error.response.data.errorList)}`)
-        console.log(error.response.status)
-        console.log(error.response.headers)
-        return error.response.data.errorList
-      }else if (error.request){
-        console.log(error.request)
-        return error.request
-      }else{
-        console.log(error.message)
-        return error.message
-      }
-    }
-  }
+			return data
+		} catch (error) {
+			if (error.response) {
+				console.log(error.response.data)
+				console.log(`ErrorList ${JSON.stringify(error.response.data.errorList)}`)
+				console.log(error.response.status)
+				console.log(error.response.headers)
+				return error.response.data.errorList
+			} else if (error.request) {
+				console.log(error.request)
+				return error.request
+			} else {
+				console.log(error.message)
+				return error.message
+			}
+		}
+	}
 
-  return getData(options)
-  
+	return getData(options)
+
 }
