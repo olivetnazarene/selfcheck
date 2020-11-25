0011d89f4b6c4fbda5a8a6ca2c4b36d72200faf5
James Cuénod <j3frea+gitkraken@gmail.com>
2020-11-25 17:27:12 -0600

    Transition AlertBox message

--- client/src/components/AlertBox.jsx
+++ client/src/components/AlertBox.jsx
@@ -1,11 +1,25 @@
 import { ExclamationSolidIcon } from "./Icons"
 
-const AlertBox = ({ children }) =>
-	<div class="flex flex-row items-center">
+const AlertBox = ({ children, visible }) => [
+	<style>{`
+		.--hide-alert {
+			opacity: 0;
+			transform: translateY(-10px);
+			transition: opacity 100ms ease-in-out,
+						transform 100ms ease-in-out;
+		}
+		.--show-alert {
+			opacity: 1;
+			transform: translateY(0);
+			transition: opacity 100ms ease-in-out,
+						transform 100ms ease-in-out;
+		}
+	`}</style>,
+	<div class={`flex flex-row items-center m-4 bg-red-300 rounded-lg border-2 border-red-800 p-2 ${visible ? "--show-alert" : "--hide-alert"}`}>
 		<ExclamationSolidIcon classes="w-6 h-6 text-red-800" />
 		<div class="mx-2 text-red-900">
 			{children}
 		</div>
-	</div>
+	</div>]
 
 export default AlertBox
