5ed0f04bae9c965a9e3faa21aa144e8e68b4b2f6
James Cuénod <j3frea+gitkraken@gmail.com>
2020-11-26 11:21:53 -0600

    PillBox can have no title

--- client/src/components/PillBox.jsx
+++ client/src/components/PillBox.jsx
@@ -1,7 +1,7 @@
 const PillBox = ({ title, value }) =>
-	<div class="flex items-center rounded-full border-2 border-blue-300 m-2 py-1 pl-3 pr-1 text-gray-300">
-		<div class="font-extrabold text-xs uppercase">{title}</div>
-		<div class="rounded-full w-8 h-8 bg-blue-300 flex justify-center items-center text-gray-100 ml-2">
+	<div class={"flex items-center rounded-full border-2 border-blue-300 m-2 py-1 pr-1 text-blue-200 " + (title ? "pl-3" : "pl-1")}>
+		{title && <div class="font-extrabold text-xs uppercase text-blue-100 mr-2">{title}</div>}
+		<div class="rounded-full w-8 h-8 bg-blue-300 flex justify-center items-center text-white">
 			{value}
 		</div>
 	</div>
