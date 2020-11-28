567bc8f7caab738833974e2c48c57d63a013b4fb
James Cuénod <j3frea+gitkraken@gmail.com>
2020-11-28 14:41:05 -0600

    Remove commented code

--- client/src/components/CheckoutLayout.jsx
+++ client/src/components/CheckoutLayout.jsx
@@ -51,13 +51,8 @@ const CheckoutLayout = ({ library, organization, userName, userLoans, userReques
 
 			{/* <!-- Barcode Scanner --> */}
 			<div class="flex-shrink w-3/4 mt-20">
-				<InputBox Icon={BookOpenIcon} onClick={() => alert("yay")} />
-				{/* <div class="flex">
-					<div class="flex-none flex items-center m-5">
-						<svg class="w-8 h-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
-						</svg>
-					</div>
+				<InputBox Icon={BookOpenIcon} onClick={checkoutBook} />
+			</div>
 					<input class="text-2xl bg-transparent outline-none w-full" placeholder=" Scan your book" />
 					<a href="#" class="flex-none flex items-center m-2 p-2 rounded cursor-pointer bg-blue-400 text-gray-200 hover:bg-blue-300 hover:text-white hover:shadow-md focus:bg-blue-600 focus:shadow-inner">
 						<svg class="w-10 h-10" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
