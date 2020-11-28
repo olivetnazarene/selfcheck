9305435b3f5e1db71701d123ffdd5978a0de2439
James Cuénod <j3frea+gitkraken@gmail.com>
2020-11-28 14:41:30 -0600

    Customizable AlertBox in CheckoutLayout

--- client/src/components/CheckoutLayout.jsx
+++ client/src/components/CheckoutLayout.jsx
@@ -5,9 +5,10 @@ import {
 } from "./Icons"
 import AlertBox from "./AlertBox"
 import InputBox from "./InputBox"
+import BookTable from "./BookTable"
 
 
-const CheckoutLayout = ({ library, organization, userName, userLoans, userRequests, userFines, timeout, booksCheckedOut }) =>
+const CheckoutLayout = ({ library, organization, userName, userLoans, userRequests, userFines, timeout, checkoutBook, books, showAlert, alertMessage }) =>
 	<div class="h-screen w-screen flex flex-col">
 
 		{/* <!-- Header Bar --> */}
@@ -53,16 +54,10 @@ const CheckoutLayout = ({ library, organization, userName, userLoans, userReques
 			<div class="flex-shrink w-3/4 mt-20">
 				<InputBox Icon={BookOpenIcon} onClick={checkoutBook} />
 			</div>
-					<input class="text-2xl bg-transparent outline-none w-full" placeholder=" Scan your book" />
-					<a href="#" class="flex-none flex items-center m-2 p-2 rounded cursor-pointer bg-blue-400 text-gray-200 hover:bg-blue-300 hover:text-white hover:shadow-md focus:bg-blue-600 focus:shadow-inner">
-						<svg class="w-10 h-10" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-					</a>
-				</div> */}
-			</div>
 
 			{/* <!-- Alert Dialog --> */}
-			<AlertBox visible={true} >
-				The barcode you scanned does not exist. Please scan a book that is actually real.
+			<AlertBox visible={showAlert} >
+				{alertMessage}
 			</AlertBox>
 
 			{/* <!-- Book Table --> */}
