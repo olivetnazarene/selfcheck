02d5ff0155fd6e09a843450799f395e95638d5d0
James Cuénod <j3frea+gitkraken@gmail.com>
2020-12-03 15:17:32 -0600

    Update example to reflect expectations :)

--- example_config.js
+++ example_config.js
@@ -3,14 +3,14 @@ module.exports = {
     "hostname": "https://api-na.hosted.exlibrisgroup.com",
     "apiKey": "XXXXXXXXXXXXXXXXXXXX",
     "locations": [{
-        "featureImage": "url/to/image.jpg",
+        "featureImageUrl": "url/to/image.jpg",
         "libraryNameString": "Friendly Library Name",
         "organizationNameString": "Friendly Organization Name",
         "apiLibraryName": "XXXXX",
         "apiCircDesk": "XXXXXXXXXXXXXXXX",
         "permitIpAddresses": ['127.0.0.1'],
     }, {
-        "featureImage": "url/to/image.jpg",
+        "featureImageUrl": "url/to/image.jpg",
         "libraryNameString": "Friendly Library Name",
         "organizationNameString": "Friendly Organization Name",
         "apiLibraryName": "XXXXX",
